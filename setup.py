85d04b3bed13a613b7661f20f1ead3a48d4316a6
Michal Toman <mtoman@redhat.com>
2013-11-14 14:25:37 +0100

    Add FAF files
    Signed-off-by: Michal Toman <mtoman@redhat.com>

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='FAF',
+    version='0.9-1.20131113',
+    description='FAF image for OpenShift',
+    author='ABRT Team',
+    author_email='crash-catcher@lists.fedorahosted.org',
+    url='https://github.com/mtoman/faf-openshift',
+    install_requires=['Django<=1.4'],
+)
