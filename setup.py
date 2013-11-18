617730a60d742aa971ddad307de8bf86676b9192
Michal Toman <mtoman@redhat.com>
2013-11-18 11:27:44 +0100

    add dependencies
    Signed-off-by: Michal Toman <mtoman@redhat.com>

--- setup.py
+++ setup.py
@@ -2,6 +2,27 @@
 
 from setuptools import setup
 
+REQUIRES = ['Django <= 1.4',
+            'argparse',
+            'bunch',
+            'backports.ssl_match_hostname',
+            'chardet',
+            'django-authopenid',
+            'django-dajax',
+            'django-dajaxice',
+            'django-openid',
+            'kitchen',
+            'kobo',
+            'ordereddict',
+            'psycopg2',
+            'python-bugzilla',
+            'python-fedora',
+            'python-openid',
+            'requests',
+            'six',
+            'sqlalchemy >= 0.7',
+            'urllib3']
+
 setup(
     name='FAF',
     version='0.9-1.20131113',
@@ -9,5 +30,5 @@ setup(
     author='ABRT Team',
     author_email='crash-catcher@lists.fedorahosted.org',
     url='https://github.com/mtoman/faf-openshift',
-    install_requires=['Django<=1.4'],
+    install_requires=REQUIRES,
 )
